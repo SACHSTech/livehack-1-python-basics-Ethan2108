847d76a8cbc29eba8d26954c85c0eaa792579191
Ethan2108 <75390842+Ethan2108@users.noreply.github.com>
2020-12-07 13:37:23 -0500

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,14 @@
+"""
+Name:   problem1.py
 
+Purpose: This problem requires a celsius to fahrenheit converter to help the user understand the amount of degrees celsius to fahrenheit
+
+Author: Au.E
+
+Date: 07/12/2020
+"""
+
+c = float(input("Degrees celsius: "))
+
+#Tells user the amount of degrees fahrenheit after conversion
+print(c , "degrees celsius equals", float((9/5) * (c) + (32) ), "degrees fahrenheit.")
