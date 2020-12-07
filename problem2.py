eaf6b6e3aeb063ec353aee91f834272468fcdee2
Ethan2108 <75390842+Ethan2108@users.noreply.github.com>
2020-12-07 13:40:18 -0500

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,1 +1,15 @@
+"""
+Name:   problem2.py
 
+Purpose: This problem is meant for determining how to fairly split the chicken that was won amongst the class and Mr. Fabroa.
+
+Author: Au.E
+
+Date: 07/12/2020
+"""
+#input data for amount of chicken
+chicken = int(input("how many pieces of chicken are there?"))
+students = int(input("how many students are there?"))
+distributed_pieces = round(chicken/students)
+Mr_fabroas_pieces = (chicken % students)
+print ("each student gets:", distributed_pieces, "and Mr.Fabroa gets:", Mr_fabroas_pieces)
